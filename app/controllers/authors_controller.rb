c0c68143177cd765d4569683c4ec10c80c462dd4
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-25 09:46:45 +0200

    added functionality to add authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,22 @@
 class AuthorsController < ApplicationController
+    def show
+        @author = Author.find(params[:id])
+    end
+
     def new
     end
+
+    def create
+        @author = Author.new(article_params)
+            
+        
+        @author.save
+        redirect_to @author
+        #render plain: params[:author].inspect
+    end
+
+    private
+        def article_params
+            params.require(:author).permit(:title, :text)
+        end
 end
