c1a0edbf39bfc2a21f095da2d6d17435e6ca441d
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-26 14:39:46 +0200

    added author overview

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Author index page", type: :feature do
+  it "should exist an overview over all added athors" do
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    @alan = FactoryBot.create :author
+
+    visit authors_path
+
+    expect(page).to have_text "Alan Turing"
+    expect(page).to have_text "http://wikipedia.de/Alan_Turing"
+    expect(page).to have_link "Show", href: author_path(@alan)
+    expect(page).to have_link "New", href: new_author_path
+  end
+end
