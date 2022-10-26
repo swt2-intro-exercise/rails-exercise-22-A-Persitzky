68a46238e732c5d965e9d0fb3bfd34fceed2aa4e
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-26 13:51:36 +0200

    full name working

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "Should have first_name, last_name, homepage" do
+    author = Author.new({:first_name => "Alan", :last_name => "Turing", :homepage =>"https://example.com"})
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("https://example.com")
+
+    expect(author.name).to eq("Alan Turing")
+  end
 end
