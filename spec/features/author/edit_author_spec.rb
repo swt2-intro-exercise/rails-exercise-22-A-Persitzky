39f76ac4d5d3c30f7d9c1c6d92f5def8b750d6ea
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-26 15:46:54 +0200

    edit authors now working

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,13 @@
+describe "Edit author page", type: :feature do
+    it "Should exist a page to edit authors" do
+        @author = FactoryBot.create :author
+
+        visit edit_author_path(@author)
+        fill_in "author[first_name]", with: "Bob"
+        click_button('Update Author')
+        @author.reload
+
+        expect(@author.first_name).to eq("Bob")
+
+    end
+end
