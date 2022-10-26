1693c71f1b10a79727480fdd9cd9088b11d4c009
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-26 14:23:51 +0200

    changed test homepage

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   it "Should have first_name, last_name, homepage" do
-    author = Author.new({:first_name => "Alan", :last_name => "Turing", :homepage =>"https://example.com"})
+    author = Author.new({:first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.de/Alan_Turing"})
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
-    expect(author.homepage).to eq("https://example.com")
+    expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
 
     expect(author.name).to eq("Alan Turing")
   end
