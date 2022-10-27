6975a25cabd24afadf677744cf8076e4f297fd01
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-27 10:02:38 +0200

    new papers fail with empty venue field

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
   
   it "Should fail with an empty title" do
-    @paper = Paper.new
+    @paper = Paper.new(:venue => "test", :year => 2022)
     expect(@paper).to_not be_valid
   end
   
+  it "Should fail with an empty " do
+    @paper = Paper.new(:title => "Title", :year => 2022)
+    expect(@paper).to_not be_valid
+  end
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
