a4ccf539fa989294c5f3b7cd5dc7149b72d1b028
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-10-27 09:56:46 +0200

    papers must have a title

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
+  
+  it "Should fail with an empty title" do
+    @paper = Paper.new
+    expect(@paper).to_not be_valid
+  end
+  
   pending "add some examples to (or delete) #{__FILE__}"
 end
