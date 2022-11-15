393692ae332531956e70cdac4975309b3547074a
Anton Persitzky <anton.persitzky@student.hpi.de>
2022-11-15 12:21:32 +0100

    implemented basic filtering for papers

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -5,8 +5,17 @@ class PapersController < ApplicationController
   
   # GET /papers
   def index
-    if params['year']
-      @papers = Paper.year(params['year'])
+    #if params['year']
+    #  @papers = Paper.year(params['year'])
+    #elsif params['search']
+    #  @papers = Paper.where("title LIKE ?", "%" + Paper.sanitize_sql_like(params['search']) + "%").or(Paper.where("venue LIKE ?", "%" + Paper.sanitize_sql_like(params['search']) + "%"))
+    #else
+    if params[:search]
+      # split search term by words into list.
+      all_params = params[:search].split(" ")
+      # for each word and attribute create like clause. Disjuncts attributes and conjucts search_terms.
+      sql_like_clause = all_params.map{|param| relevant_search_attributes.map { |attribute| create_sql_like(param, attribute) }.join(" OR ") }.join(" AND ")
+      @papers = Paper.where(sql_like_clause)
     else
       @papers = Paper.all
     end
@@ -61,4 +70,20 @@ class PapersController < ApplicationController
     def paper_params
       params.require(:paper).permit(:title, :venue, :year, :author_ids => [])
     end
+
+    # returns LIKE partial matching search
+    def create_sql_like(search_term, search_attribute)
+      "#{search_attribute} LIKE '%#{search_term}%'"
+    end
+
+    # retuns equals expression for filter
+    def create_sql_equal(search_value, search_attribute)
+      "#{search_attribute} = '#{search_value}'"
+    end
+
+    def relevant_search_attributes
+      [:title, :venue]
+    end
+
+
 end
